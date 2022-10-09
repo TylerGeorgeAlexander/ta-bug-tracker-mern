034ba24aa348a079c60960ec86e99c95179721b1
Tyler Alexander <TylerGeorgeAlexander@gmail.com>
2022-10-08 19:12:44 -0700

    darkTheme default set to light

--- mern-auth-client/tailwind.config.js
+++ mern-auth-client/tailwind.config.js
@@ -4,5 +4,18 @@ module.exports = {
   theme: {
     extend: {},
   },
+  // add daisyUI plugin
   plugins: [require("daisyui")],
+
+  // daisyUI config (optional)
+  daisyui: {
+    styled: true,
+    themes: true,
+    base: true,
+    utils: true,
+    logs: true,
+    rtl: false,
+    prefix: "",
+    darkTheme: "light",
+  },
 };

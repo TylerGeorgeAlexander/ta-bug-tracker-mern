5657f01f37bd9a94d39031f575309f7fdeb6e5b4
Tyler Alexander <TylerGeorgeAlexander@gmail.com>
2022-10-13 01:31:06 -0700

    added profile id link with correct user params

--- mern-auth-client/src/components/Navbar.jsx
+++ mern-auth-client/src/components/Navbar.jsx
@@ -128,7 +128,10 @@ const Navbar = () => {
                 className="mt-3 p-2 shadow menu menu-compact dropdown-content bg-base-100 rounded-box w-52"
               >
                 <li>
-                  <Link to="/profile" className="justify-between">
+                  <Link
+                    to={`/profile/${userContext.details?._id}`}
+                    className="justify-between"
+                  >
                     Profile
                     <span className="badge">New</span>
                   </Link>

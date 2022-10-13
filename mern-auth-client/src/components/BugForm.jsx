73726f981960a87493fa7dda9fd30ec44bacd9c7
Tyler G Alexander <tylergeorgealexander@gmail.com>
2022-10-13 14:57:55 -0700

    style and error message

--- mern-auth-client/src/components/BugForm.jsx
+++ mern-auth-client/src/components/BugForm.jsx
@@ -8,6 +8,9 @@ const BugForm = ({ userContext }) => {
     description: "",
     priority: "low",
   });
+
+  const [error, setError] = useState("");
+
   // const [name, setName] = useState("");
   // const [desc, setDesc] = useState("");
   // const [priority, setPriority] = useState("");
@@ -68,11 +71,35 @@ const BugForm = ({ userContext }) => {
       .catch(function (response) {
         //handle error
         console.log(response);
+        setError(response);
       });
   };
 
   return (
     <>
+      {/* ERROR ALERT */}
+      {error && (
+        <div className="flex justify-center m-4">
+          <div className="alert alert-error shadow-lg w-1/2 justify-center">
+            <div>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="stroke-current flex-shrink-0 h-6 w-6"
+                fill="none"
+                viewBox="0 0 24 24"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"
+                />
+              </svg>
+              <span>{error.message}</span>
+            </div>
+          </div>
+        </div>
+      )}
       {/* <h1>{userContext.details._id && userContext.details._id}</h1> */}
       <form onSubmit={submitForm} id="testForm">
         <div className="flex justify-center m-4">

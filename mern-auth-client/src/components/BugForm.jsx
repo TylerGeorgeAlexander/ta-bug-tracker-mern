fd0ee7d0b209b916121a3923d3cf689a32ad02ad
Tyler Alexander <TylerGeorgeAlexander@gmail.com>
2022-10-13 03:12:08 -0700

    styles

--- mern-auth-client/src/components/BugForm.jsx
+++ mern-auth-client/src/components/BugForm.jsx
@@ -74,60 +74,95 @@ const BugForm = ({ userContext }) => {
     <>
       {/* <h1>{userContext.details._id && userContext.details._id}</h1> */}
       <form onSubmit={submitForm} id="testForm">
-        <label htmlFor="name" className="label-text">
-          Name:{" "}
-        </label>
-        <input
-          id="name"
-          name="name"
-          type="text"
-          value={bodyData.name}
-          onChange={(e) => setBodyData({ ...bodyData, name: e.target.value })}
-          className="input input-bordered w-full max-w-xs"
-        />
-        <label htmlFor="desc" className="label-text">
-          Desc:{" "}
-        </label>
-        <input
-          id="desc"
-          name="desc"
-          type="text"
-          value={bodyData.description}
-          onChange={(e) =>
-            setBodyData({ ...bodyData, description: e.target.value })
-          }
-          className="input input-bordered w-full max-w-xs"
-        />
-        <label htmlFor="priority" className="label-text">
-          Priority:{" "}
-        </label>
+        <div className="flex justify-center m-4">
+          <label htmlFor="name" className="label-text text-lg">
+            Name:{" "}
+          </label>
+        </div>
+        <div className="flex justify-center m-4">
+          <input
+            id="name"
+            name="name"
+            type="text"
+            value={bodyData.name}
+            onChange={(e) => setBodyData({ ...bodyData, name: e.target.value })}
+            className="input input-bordered w-full max-w-xs"
+          />
+        </div>
+        <div className="flex justify-center m-4">
+          <label htmlFor="desc" className="label-text text-lg">
+            Description:{" "}
+          </label>
+        </div>
+        <div className="flex justify-center m-4">
+          <input
+            id="desc"
+            name="desc"
+            type="text"
+            value={bodyData.description}
+            onChange={(e) =>
+              setBodyData({ ...bodyData, description: e.target.value })
+            }
+            className="input input-bordered w-full max-w-xs"
+          />
+        </div>
+        <div className="flex justify-center m-4">
+          <label htmlFor="priority" className="label-text text-lg">
+            Priority:{" "}
+          </label>
+        </div>
+        <div className="flex justify-center m-4">
+          <select
+            id="priority"
+            name="priority"
+            value={bodyData.priority}
+            onChange={(e) =>
+              setBodyData({ ...bodyData, priority: e.target.value })
+            }
+            className="select select-bordered w-full max-w-xs"
+          >
+            <option disabled>Priority Level:</option>
+            <option value="low">Low</option>
+            <option value="medium">Medium</option>
+            <option value="high">High</option>
+            <option value="immediately">Immediately</option>
+          </select>
+        </div>
+        <div className="flex justify-center m-4">
+          <label className="w-64 flex flex-col items-center px-4 py-6 bg-white text-blue rounded-lg shadow-lg tracking-wide uppercase border border-blue cursor-pointer hover:bg-white hover:text-secondary">
+            <svg
+              className="w-8 h-8"
+              fill="currentColor"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 20 20"
+            >
+              <path d="M16.88 9.1A4 4 0 0 1 16 17H5a5 5 0 0 1-1-9.9V7a3 3 0 0 1 4.52-2.59A4.98 4.98 0 0 1 17 8c0 .38-.04.74-.12 1.1zM11 11h3l-4-4-4 4h3v3h2v-3z" />
+            </svg>
+            <span className="mt-2 text-base leading-normal">Select a file</span>
+            <input
+              type="file"
+              name="file"
+              id="file"
+              value={filePath}
+              onChange={(e) => setFilePath(e.target.value)}
+              className="hidden"
+            />
+          </label>
 
-        <select
-          id="priority"
-          name="priority"
-          value={bodyData.priority}
-          onChange={(e) =>
-            setBodyData({ ...bodyData, priority: e.target.value })
-          }
-          className="select select-bordered w-full max-w-xs"
-        >
-          <option disabled>Priority Level:</option>
-          <option value="low">Low</option>
-          <option value="medium">Medium</option>
-          <option value="high">High</option>
-          <option value="immediately">Immediately</option>
-        </select>
-
-        <input
-          type="file"
-          name="file"
-          id="file"
-          value={filePath}
-          onChange={(e) => setFilePath(e.target.value)}
-        />
-        <button className="btn btn-primary" type="submit">
-          Submit
-        </button>
+          {/* OG */}
+          {/* <input
+            type="file"
+            name="file"
+            id="file"
+            value={filePath}
+            onChange={(e) => setFilePath(e.target.value)}
+          /> */}
+        </div>
+        <div className="flex justify-center m-4">
+          <button className="btn btn-primary" type="submit">
+            Submit
+          </button>
+        </div>
       </form>
       {/* <button className="btn btn-secondary" type="click" onClick={clickTest}>
         Click Test

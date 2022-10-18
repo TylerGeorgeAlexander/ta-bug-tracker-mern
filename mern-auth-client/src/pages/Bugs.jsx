d2bc94ae3592e3da00fae53a76d80f96e54e8366
Tyler G Alexander <tylergeorgealexander@gmail.com>
2022-10-17 17:19:25 -0700

    front end created by filter

--- mern-auth-client/src/pages/Bugs.jsx
+++ mern-auth-client/src/pages/Bugs.jsx
@@ -10,6 +10,11 @@ import PriorityPill from "../components/PriorityPill";
 
 const Bugs = ({ userContext }) => {
   const [data, setData] = useState([]);
+  const [filter, setFilter] = useState({
+    assignedTo: "",
+    createdBy: "",
+    type: "",
+  });
 
   const getData = async () => {
     const UPLOAD_ENDPOINT = "http://localhost:8081/bug/getFeed";
@@ -63,9 +68,30 @@ const Bugs = ({ userContext }) => {
 
   return (
     <>
-      {/* data structure stringified
+      data structure stringified
       <h2>Bugs</h2>
-      <div>{JSON.stringify(data)}</div> */}
+      {/* <div>{JSON.stringify(data)}</div> */}
+      <p>{JSON.stringify(filter)}</p>
+      <div className="flex justify-center">
+        <label htmlFor="createdBy"> Created By: </label>
+        <select
+          id="createdBy"
+          name="createdBy"
+          value={filter.createdBy}
+          required
+          onChange={(e) => setFilter({ ...filter, createdBy: e.target.value })}
+          className="select select-bordered w-full max-w-xs"
+        >
+          <option value="">ALL</option>
+          {data &&
+            data.users?.map((user) => (
+              <option
+                key={user._id}
+                value={user._id}
+              >{`${user.firstName} ${user.lastName}`}</option>
+            ))}
+        </select>
+      </div>
       <div className="overflow-x-auto w-full">
         <table className="table w-full">
           {/* <!-- head --> */}
@@ -90,137 +116,141 @@ const Bugs = ({ userContext }) => {
           <tbody>
             {/* <!-- row 1 --> */}
             {data &&
-              data.bugs?.map((bug) => {
-                return (
-                  <tr key={`${bug._id} tr`} className="hover">
-                    <th>
-                      <label>
-                        <input
-                          type="checkbox"
-                          className="checkbox"
-                          // Checked off if Bug is resolved
-                          checked={bug.resolved}
-                          readOnly
-                        />
-                      </label>
-                    </th>
-                    <td>
-                      <div className="flex items-center space-x-3 whitespace-normal">
-                        <div>
-                          <div className="font-bold">{bug.name}</div>
-                          <div className="text-sm opacity-50 text-center">
-                            {bug.app || "Default Application"}
+              data.bugs
+                ?.filter((bug) =>
+                  filter.createdBy ? bug.user._id === filter.createdBy : bug
+                )
+                .map((bug) => {
+                  return (
+                    <tr key={`${bug._id} tr`} className="hover">
+                      <th>
+                        <label>
+                          <input
+                            type="checkbox"
+                            className="checkbox"
+                            // Checked off if Bug is resolved
+                            checked={bug.resolved}
+                            readOnly
+                          />
+                        </label>
+                      </th>
+                      <td>
+                        <div className="flex items-center space-x-3 whitespace-normal">
+                          <div>
+                            <div className="font-bold">{bug.name}</div>
+                            <div className="text-sm opacity-50 text-center">
+                              {bug.app || "Default Application"}
+                            </div>
                           </div>
                         </div>
-                      </div>
-                    </td>
-                    <td>
-                      <div className="flex items-center space-x-3 whitespace-normal justify-center">
-                        {bug.description}
-                      </div>
-                    </td>
-                    <td className="text-center">
-                      <PriorityPill priority={bug.priority} />
-                    </td>
-                    <td>
-                      <div className="flex items-center space-x-3 justify-center">
-                        <div className="avatar">
-                          <div className="mask mask-squircle w-12 h-12">
-                            <img
-                              src={bug.image || "favicon.ico"}
-                              alt="Bug's Avatar"
-                            />
+                      </td>
+                      <td>
+                        <div className="flex items-center space-x-3 whitespace-normal justify-center">
+                          {bug.description}
+                        </div>
+                      </td>
+                      <td className="text-center">
+                        <PriorityPill priority={bug.priority} />
+                      </td>
+                      <td>
+                        <div className="flex items-center space-x-3 justify-center">
+                          <div className="avatar">
+                            <div className="mask mask-squircle w-12 h-12">
+                              <img
+                                src={bug.image || "favicon.ico"}
+                                alt="Bug's Avatar"
+                              />
+                            </div>
                           </div>
                         </div>
-                      </div>
-                    </td>
+                      </td>
 
-                    <td>
-                      <div className="flex items-center space-x-3 justify-center">
-                        <div className="grid text-center">
-                          <div className="grid-cols-1">
-                            <span className="text-center font-semibold">
-                              {bug.createdBy}
-                            </span>
-                          </div>
-                          <div className="grid-cols-1 m-2">
-                            {bug.user.profilePicture ? (
-                              <Avatar
-                                picture={bug.user.profilePicture}
-                                firstName={bug.user.firstName}
-                                lastName={bug.user.lastName}
-                              />
-                            ) : (
-                              <AvatarPlaceholder
-                                firstName={bug.user.firstName}
-                                lastName={bug.user.lastName}
-                              />
-                            )}
-                          </div>
-                          <div className="grid-cols-1">
-                            <span className="badge badge-ghost badge-sm">
-                              {bug.user?.role || "Software Engineer I"}
-                            </span>
-                          </div>
-                        </div>
-                      </div>
-                    </td>
-
-                    <td className="text-center">
-                      <div className="flex items-center space-x-3 justify-center">
-                        <div className="grid text-center">
-                          <div className="grid-cols-1">
-                            <span className="text-center font-semibold">
-                              {bug.assignedTo}
-                            </span>
-                          </div>
-                          <div className="grid-cols-1 m-2">
-                            {bug.assignedTo ? (
-                              <Avatar
-                                picture={bug.user.profilePicture}
-                                firstName={bug.user.firstName}
-                                lastName={bug.user.lastName}
-                              />
-                            ) : (
-                              <AvatarPlaceholder
-                                firstName={"N"}
-                                lastName={"A"}
-                              />
-                            )}
-                          </div>
-                          <div className="grid-cols-1">
-                            {bug.user.role && (
-                              <span className="badge badge-ghost badge-sm">
-                                {bug.user?.role}
+                      <td>
+                        <div className="flex items-center space-x-3 justify-center">
+                          <div className="grid text-center">
+                            <div className="grid-cols-1">
+                              <span className="text-center font-semibold">
+                                {bug.createdBy}
                               </span>
-                            )}
+                            </div>
+                            <div className="grid-cols-1 m-2">
+                              {bug.user.profilePicture ? (
+                                <Avatar
+                                  picture={bug.user.profilePicture}
+                                  firstName={bug.user.firstName}
+                                  lastName={bug.user.lastName}
+                                />
+                              ) : (
+                                <AvatarPlaceholder
+                                  firstName={bug.user.firstName}
+                                  lastName={bug.user.lastName}
+                                />
+                              )}
+                            </div>
+                            <div className="grid-cols-1">
+                              <span className="badge badge-ghost badge-sm">
+                                {bug.user?.role || "Software Engineer I"}
+                              </span>
+                            </div>
                           </div>
                         </div>
-                      </div>
-                    </td>
+                      </td>
 
-                    <td className="text-center">
-                      <Link
-                        to={`/details/${bug._id}`}
-                        className="btn btn-ghost btn-xs"
-                      >
-                        details
-                      </Link>
-                    </td>
+                      <td className="text-center">
+                        <div className="flex items-center space-x-3 justify-center">
+                          <div className="grid text-center">
+                            <div className="grid-cols-1">
+                              <span className="text-center font-semibold">
+                                {bug.assignedTo}
+                              </span>
+                            </div>
+                            <div className="grid-cols-1 m-2">
+                              {bug.assignedTo ? (
+                                <Avatar
+                                  picture={bug.user.profilePicture}
+                                  firstName={bug.user.firstName}
+                                  lastName={bug.user.lastName}
+                                />
+                              ) : (
+                                <AvatarPlaceholder
+                                  firstName={"N"}
+                                  lastName={"A"}
+                                />
+                              )}
+                            </div>
+                            <div className="grid-cols-1">
+                              {bug.user.role && (
+                                <span className="badge badge-ghost badge-sm">
+                                  {bug.user?.role}
+                                </span>
+                              )}
+                            </div>
+                          </div>
+                        </div>
+                      </td>
 
-                    <td className="text-center">
-                      <div className="m-2">
-                        <ResolveModal id={bug._id} resolveBug={resolveBug} />
-                      </div>
-                      <div className="m-2">
-                        {userContext.details?.rank === 1 && (
-                          <DeleteModal id={bug._id} deleteBug={deleteBug} />
-                        )}
-                      </div>
-                    </td>
-                  </tr>
-                );
-              })}
+                      <td className="text-center">
+                        <Link
+                          to={`/details/${bug._id}`}
+                          className="btn btn-ghost btn-xs"
+                        >
+                          details
+                        </Link>
+                      </td>
+
+                      <td className="text-center">
+                        <div className="m-2">
+                          <ResolveModal id={bug._id} resolveBug={resolveBug} />
+                        </div>
+                        <div className="m-2">
+                          {userContext.details?.rank === 1 && (
+                            <DeleteModal id={bug._id} deleteBug={deleteBug} />
+                          )}
+                        </div>
+                      </td>
+                    </tr>
+                  );
+                })}
             {/* tbody end */}
           </tbody>
           {/* <!-- foot --> */}

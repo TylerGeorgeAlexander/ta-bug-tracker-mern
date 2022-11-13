106c7f0bc85b6fe37b75e4b204a74079bbf46601
Tyler G Alexander <tylergeorgealexander@gmail.com>
2022-11-12 21:44:10 -0800

    chore: loading updates

--- mern-auth-client/src/components/Loader.jsx
+++ mern-auth-client/src/components/Loader.jsx
@@ -3,9 +3,14 @@ import React from "react";
 
 const Loader = () => {
   return (
-    <div className="loader">
-      <Spinner size={50} />
-    </div>
+    <>
+      <div className="loader">
+        <Spinner size={50} />
+      </div>
+      <div className="flex justify-center">
+        <span className="text-center">Current hosting needs to be a few minutes to spin up.</span>
+      </div>
+    </>
   );
 };
 
